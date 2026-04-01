4a9f2a36b6b9c6769f52736971b40875341ca2e2
prokenheart <2003nhphuc@gmail.com>
2026-04-02 01:49:50 +0700

    feat: get name from local

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button, TextField, Stack, CircularProgress } from "@mui/material";
 import axios from "axios";
 import WeightedWheel from "./WeightedWheel";
@@ -15,6 +15,70 @@ const data = [
 type dataType = {
   option: string;
   weight: number;
+};
+
+type soundDataType = {
+  label: string;
+  path: string;
+  place: number;
+  style: string;
+};
+
+type existingDataType = {
+  sound1: soundDataType | null;
+  sound2: soundDataType | null;
+  sound3: soundDataType | null;
+  sound4: soundDataType | null;
+  sound5: soundDataType | null;
+  sound6: soundDataType | null;
+  sound7: soundDataType | null;
+  sound8: soundDataType | null;
+  sound9: soundDataType | null;
+  sound10: soundDataType | null;
+
+  sound11: soundDataType | null;
+  sound12: soundDataType | null;
+  sound13: soundDataType | null;
+  sound14: soundDataType | null;
+  sound15: soundDataType | null;
+  sound16: soundDataType | null;
+  sound17: soundDataType | null;
+  sound18: soundDataType | null;
+  sound19: soundDataType | null;
+  sound20: soundDataType | null;
+
+  sound21: soundDataType | null;
+  sound22: soundDataType | null;
+  sound23: soundDataType | null;
+  sound24: soundDataType | null;
+  sound25: soundDataType | null;
+  sound26: soundDataType | null;
+  sound27: soundDataType | null;
+  sound28: soundDataType | null;
+  sound29: soundDataType | null;
+  sound30: soundDataType | null;
+
+  sound31: soundDataType | null;
+  sound32: soundDataType | null;
+  sound33: soundDataType | null;
+  sound34: soundDataType | null;
+  sound35: soundDataType | null;
+  sound36: soundDataType | null;
+  sound37: soundDataType | null;
+  sound38: soundDataType | null;
+  sound39: soundDataType | null;
+  sound40: soundDataType | null;
+
+  sound41: soundDataType | null;
+  sound42: soundDataType | null;
+  sound43: soundDataType | null;
+  sound44: soundDataType | null;
+  sound45: soundDataType | null;
+  sound46: soundDataType | null;
+  sound47: soundDataType | null;
+  sound48: soundDataType | null;
+  sound49: soundDataType | null;
+  sound50: soundDataType | null;
 };
 
 const allowedExtensions = [".mp3"];
@@ -81,6 +145,8 @@ function App() {
   const [prizeStyle, setPrizeStyle] = useState<string | null>(null);
   const [view, setView] = useState<number>(1);
 
+  const [existingData, setExistingData] = useState<existingDataType>();
+
   const mapingPrizeToStyle = (option: number) => {
     switch (option) {
       case 0:
@@ -110,6 +176,21 @@ function App() {
         return "#5865F2";
     }
   };
+
+  const mappingStyleToColor = (style: string) => {
+    switch (style) {
+      case "primary":
+        return "#5865F2";
+      case "success":
+        return "#248046";
+      case "secondary":
+        return "#4E5058";
+      case "danger":
+        return "#DA373C";
+      default:
+        return "#5865F2";
+    }
+  }
 
   const getWeightedIndex = (data: dataType[]) => {
     const total = data.reduce((sum, item) => sum + item.weight, 0);
@@ -142,7 +223,23 @@ function App() {
     setSoundName("");
     setPrizeIndex(0);
     setPrizeStyle(null);
+    fetchData(); // Refresh data after upload
   };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setExistingData(response.data.data);
+    } catch (err) {
+      console.error("Failed to fetch data:", err);
+    }
+  };
+
+  console.log("Existing data state:", existingData?.sound1);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const handleUpload = async () => {
     setIsUploading(true);
@@ -207,37 +304,63 @@ function App() {
         }}
       >
         {view === 1 &&
-          buttons.map((num) => (
-            <Button
-              key={num}
-              variant="contained"
-              onClick={() => setSelectedButton(num)}
-              style={{
-                backgroundColor: selectedButton === num ? "red" : "",
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
-              Button {num}
-            </Button>
-          ))}
+          buttons.map((num) => {
+            if (!existingData) return null;
+
+            const key = `sound${num}` as keyof existingDataType;
+            const label = existingData[key]?.label;
+            const style = existingData[key]?.style;
+
+            return (
+              <Button
+                key={num}
+                variant= "contained"
+                onClick={() => setSelectedButton(num)}
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  backgroundColor: selectedButton === num ? "yellow" : mappingStyleToColor(style ?? ""),
+                  color: selectedButton === num ? "black" : "white",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  textTransform: "none",
+                }}
+              >
+                {label ?? "Button " + num}
+              </Button>
+            );
+          })}
         {view === 2 &&
-          buttons2.map((num) => (
-            <Button
-              key={num}
-              variant="contained"
-              onClick={() => setSelectedButton(num)}
-              style={{
-                backgroundColor: selectedButton === num ? "red" : "",
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
-              Button {num}
-            </Button>
-          ))}
+          buttons2.map((num) => {
+            if (!existingData) return null;
+
+            const key = `sound${num}` as keyof existingDataType;
+            const label = existingData[key]?.label;
+            const style = existingData[key]?.style;
+
+            return (
+              <Button
+                key={num}
+                variant="contained"
+                onClick={() => setSelectedButton(num)}
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  backgroundColor: selectedButton === num ? "yellow" : mappingStyleToColor(style ?? ""),
+                  color: selectedButton === num ? "black" : "white",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  textTransform: "none",
+                }}
+              >
+                {label ?? "Null" + num}
+              </Button>
+            );
+          })}
       </div>
       <Stack
         direction={"row"}
